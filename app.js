e8a7a3e16f81be5f73b2e223130ef15b489406e4
kmahesh-sailfish <maheshvrsecit@gamil.com>
2016-07-08 13:14:26 +0530

    forme

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var mysql=require('mysql')
+var mysql=require('mysql');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var pool=mysql.createPool({
@@ -11,7 +11,7 @@ var pool=mysql.createPool({
   password: 'xjmpll55ycvz1mmc',
   database: 'rrywvyxe9mhors94',
   ConnectionLimit: 20
-})
+});
 /*var routes = require('./routes/index');*/
 var users = require('./routes/users')(pool);
 
@@ -52,6 +52,10 @@ app.use(function (req, res, next) {
   // Pass to next layer of middleware
   next();
 });
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -64,7 +68,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -75,7 +79,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,

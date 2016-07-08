e65683882ab5bf00e1f20906e0de18dd3cf8d6d1
kmahesh-sailfish <maheshvrsecit@gamil.com>
2016-07-08 12:38:16 +0530

    vsp code server side

---
+++ routes/users.js
@@ -0,0 +1,124 @@
+var express = require('express');
+var router =function(pool){
+  var accountRouter=express.Router();
+
+  /* GET users listing. */
+  accountRouter.route('/display').get(function(req, res, next) {
+    var query='select * from Users';
+    pool.getConnection(function(err,connection){
+      connection.query(query,function(err,rows){
+        connection.release();
+        if(err){
+          console.log(err);
+          res.status(500).send('500 Error'+err);
+        }
+        else
+        {
+          res.status(200).json(rows);
+        }
+      })
+    })
+
+  });
+  accountRouter.route('/inseruser').post(function(req, res, next) {
+    var query="insert into Users(userName,userLastname,useraddress,useremail) values"+"('" +req.body['userName']+"','"+req.body['userLastname']+"','"+req.body['useraddress']+"','"+req.body['useremail']+"')";
+    pool.getConnection(function(err,connection){
+      connection.query(query,function(err,rows){
+        connection.release();
+        if(err){
+          console.log(err);
+          res.status(500).send('500 Error'+err);
+        }
+        else
+        {
+          res.status(200).json(rows);
+        }
+      })
+    })
+  });
+
+  accountRouter.route('/getuserbyid').get(function(req,res,next)
+  {
+
+    console.log(req.query.idUsers);
+    pool.getConnection(function (err, connection)
+    {
+
+      connection.release();
+      if (req.query.idUsers)
+      {
+        var query = "select * from Users where idUsers= '" + req.query.idUsers + "'";
+        console.log('queryt' + query);
+        connection.query(query, function (err, rows)
+        {
+          if (err)
+          {
+            console.log(err);
+            res.status(500).send('500 Error :' + err);
+          }
+          else
+          {
+            console.log(rows);
+            res.status(200).json(rows);
+          }
+        });
+      }
+      else
+      {
+        console.log('id is undefind')
+      }
+    })
+  });
+
+  accountRouter.route('/updateuser').put(function(req,res,next){
+    if (req.query.idUsers) {
+      var query="UPDATE Users SET userName='"+req.body['userName']+"'where idUsers= '"+req.query.idUsers+"'";
+      pool.getConnection(function(err,connection){
+        connection.query(query,function(err,rows){
+          connection.release();
+          if(err)
+          {
+            console.log(err);
+            res.status(500).send('500 Error'+err);
+
+          }
+          else
+          {
+            res.status(200).json(rows);
+          }
+        })
+      })
+    }
+
+  })
+  accountRouter.route('/delete').delete(function(req,res,next)
+  {
+    pool.getConnection(function (err, connection)
+    {
+      connection.release();
+      if (req.query.idUsers)
+      {
+        console.log(req.body);
+        var query = "delete from Users where idUsers ='" + req.query.idUsers + "'";
+        console.log('queryt' + query);
+        connection.query(query, function (err, rows)
+        {
+          if (err)
+          {
+            console.log(err);
+            res.status(500).send('500 Error :' + err);
+          }
+          else
+          {
+            console.log(rows);
+            res.status(200).json(rows);
+          }
+        });
+      }
+    })
+  });
+ return accountRouter
+}
+
+
+module.exports = router;
